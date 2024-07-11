4a52fd34d5bcf48de19b7fa2a6d64ab55f65506f
TADIO1829 <151254512+TADIO1829@users.noreply.github.com>
2024-07-10 22:13:42 -0500

    Primero

---
+++ src/Estudiantes.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Estudiantes {
+    private JPanel panel1;
+    private JTextArea IngresoNombre;
+    private JTextField textField1;
+    private JTextArea IngresoCedula;
+    private JTextField textField2;
+    private JButton cedulaButton;
+    private JButton nombreButton;
+    private JLabel busqueda;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame = new JFrame("Mi aplicación");
+        frame.setContentPane(new Estudiantes().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
